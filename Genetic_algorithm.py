6d7b894bde97b8e0abefd7161be56924ac1ca62b
Sumanth Balaji <sumanth.goku@gmail.com>
2020-03-21 16:23:21 +0530

    fixed

--- Genetic_algorithm.py
+++ Genetic_algorithm.py
@@ -12,7 +12,7 @@ def cal_pop_fitness(vector):
     # Calculating the fitness value of each solution in the current population.
     for itr in range( population_number ):
         temp = list( vector[itr][1:] )  # take each of the genes
-        
+
         err = client.get_errors( secret_key, temp )
         vector[itr][0] = 0.5*err[0] + 1.2*err[1]  # taking fitness as the sum of the errors
         print("query number:- ",itr, vector[itr][0])
@@ -71,42 +71,33 @@ def mutation(offspring, parents, offspring_no,parents_no):
     return np.append(parents,offspring,axis=0)
 
 def initialize_population():  # load previous population as per status
-    return np.loadtxt( "./saved_populations.txt", delimiter=',' )
+    # returnable = np.loadtxt( "./saved_populations.txt", delimiter=',' )
+    returnable = np.loadtxt( "./saved_populations.txt", delimiter=',' )
 
+    with open("./saved_populations.txt","w") as f:
+        pass #to truncate the saved_populations file
 
-def save_population(vector):  # saves the population that is passed in and the corresponding error rate for them
-    np.savetxt( './saved_populations.txt', vector, delimiter=',' )
-
-def generation():
-    overweight=[0.0, 0.1240317450077846, -6.211941063144333, 0.04933903144709126, 0.03810848157715883,
-                                          8.132366097133624e-05, -6.018769160916912e-05, -1.251585565299179e-07,
-                                          3.484096383229681e-08, 4.1614924993407104e-11, -6.732420176902565e-12]
-    err = client.get_errors( secret_key, overweight )
-    sum=err[1]
-    overweight.insert(0,sum)
-    page = np.empty( (40, 12) )
-    for itr in range(40):
-        for ind,x in enumerate(overweight):
-            page[itr][ind]=x
-    save_population(page)
+    return returnable
 
 if __name__ == "__main__":
     vector = initialize_population()
     initial = vector
     print("Population has been loaded")
+
     parents = select_mating_pool( vector, 20 )
     offspring = crossover( parents, 20 )
     temp=mutation(offspring,parents,20,20)
     print("New population has been created")
+
     new=np.append(np.zeros((40,1)),temp,axis=1)
     print("starting fitness measure now")
+
     cal_pop_fitness(new)
     print("fitness measured")
+
     total_population=np.append(initial,new,axis=0)
-    np.argsort( total_population[:, 0] )
-    #save_population(total_population)
-    
+    total_population.sort(axis=0)
+
     final_population=total_population[:40,:] #choosing top 40
-    save_population(final_population)
-    print("population has been saved")
-    generation()
+
+    np.savetxt( './saved_populations.txt', final_population, delimiter=',' )
